e6dac2a2f827ccaccea6353866d2f1101418c1f0
JoaquinM8 <joaquiloquito@gmail.com>
2025-05-09 09:19:41 +0200

    DHTML

---
+++ Lab. Aplicaciones/HTML/5-9 DHTML/js/hola.js
@@ -0,0 +1,10 @@
+document.getElementById("titulo").innerHTML = "Paplata"
+document.getElementById("titulo").style.color = "#445898"
+
+let num1 = parseInt(prompt("N1 > "))
+let num2 = parseInt(prompt("N2 > "))
+
+sum = num1 + num2
+
+document.getElementById("suma").innerHTML = "La suma es: " + sum
+document.getElementById("suma").style.color = "#449852"
