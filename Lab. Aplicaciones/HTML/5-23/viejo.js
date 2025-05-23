5830119855730d5f94918802597d77f5c1013b80
JoaquinM8 <joaquiloquito@gmail.com>
2025-05-23 09:31:25 -0300

    js

--- Lab. Aplicaciones/HTML/5-23/viejo.js
+++ Lab. Aplicaciones/HTML/5-23/viejo.js
@@ -6,5 +6,10 @@ console.log("Nombre: " + nombre)
 console.log("Clave: " + clavedep)
 
 document.getElementById("nombre").innerHTML = `Nombre: ${nombre}`
+document.getElementById("nombre").style.color = "#17202a"
+
 document.getElementById("clave").innerHTML = `Clave de departamento: ${clavedep}`
+document.getElementById("clave").style.color = "#0b5345"
+
 document.getElementById("dias").innerHTML = `Antigüedad: ${años}`
+document.getElementById("dias").style.color = "#154360"
